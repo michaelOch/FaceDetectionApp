abd2c446f210bb975ab411bd0c45efcfd394d5f8
Ochuenwike Chukwudi <ochuenwikechukwudi@hotmail.com>
2020-04-03 21:50:49 -0700

    Updates

---
+++ precache-manifest.c35a840a571930ef6f7705efc3d5c08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e97e34aabf584ba7baf60bde0f5fe41",
+    "url": "/FaceDetectionApp/index.html"
+  },
+  {
+    "revision": "cf56c47931c393b65a22",
+    "url": "/FaceDetectionApp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f163b9af76ba88ca1847",
+    "url": "/FaceDetectionApp/static/css/main.ba522e38.chunk.css"
+  },
+  {
+    "revision": "cf56c47931c393b65a22",
+    "url": "/FaceDetectionApp/static/js/2.df1335a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceDetectionApp/static/js/2.df1335a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f163b9af76ba88ca1847",
+    "url": "/FaceDetectionApp/static/js/main.a8abca5d.chunk.js"
+  },
+  {
+    "revision": "aff99748532b23d39cbe",
+    "url": "/FaceDetectionApp/static/js/runtime-main.657137ec.js"
+  }
+]);
